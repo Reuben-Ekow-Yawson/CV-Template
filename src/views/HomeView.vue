85e73f78940f05c7f851142a9a1a5122cdecadb9
Reuben Ekow Yawson <87475624+Reuben-Ekow-Yawson@users.noreply.github.com>
2022-11-10 14:15:55 +0000

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import CVTemplate from '../components/CVTemplate.vue';
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <CVTemplate />
+  </main>
+</template>
